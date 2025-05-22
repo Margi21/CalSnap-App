df87f6a7d26cd78dd7c1d0b288e0067ade1123cc
Margi21 <margipalan21@gmail.com>
2025-05-22 23:08:13 +0530

    - init

---
+++ CalSnap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalSnap
+//
+//  Created by Margi Palan on 22/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
